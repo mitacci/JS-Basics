90eca5664ca29b16e59afac008a29f00632eeb12
Dimitar Stoyanov <97186457+mitacci@users.noreply.github.com>
2022-01-16 16:12:22 +0200

    Update 01_SumSeconds.js

--- ConditionalStatements/Exercise/01_SumSeconds.js
+++ ConditionalStatements/Exercise/01_SumSeconds.js
@@ -26,3 +26,25 @@ if (seconds < 10) {
     console.log(`${minutes}:${seconds}`);
 }
 }
+//=================================================
+function sumSeconds(input){
+    const firstTime = Number(input[0]); 
+    const secondTime = Number(input[1]); 
+    const thirdTime = Number(input[2]); 
+    
+    const totalTime = firstTime + secondTime + thirdTime;
+
+    const minutes = totalTime / 60;
+    const seconds = totalTime % 60;
+  
+if (seconds < 10) {
+    console.log(`${Math.floor(minutes)}:0${seconds}`);
+    
+} else {
+    console.log(`${Math.floor(minutes)}:${seconds}`);
+}
+}
+// sumSeconds(["35", "45", "44"]);
+// sumSeconds(["22", "7", "34"]);
+// sumSeconds(["50", "50", "49"]);
+// sumSeconds(["14", "12", "10"]);
